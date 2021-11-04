f49ba8c237edaf9ddab7e9a8250dd45fa34ddcb9
Vostok17 <gagarin.artem.2008@gmail.com>
2021-11-05 02:37:01 +0300

    Edit .gitignore

--- traveling salesman problem/Program.cs
+++ traveling salesman problem/Program.cs
@@ -11,19 +11,16 @@ namespace traveling_salesman_problem
             const int GRAPH_SIZE = 150;
             const int MIN = 5, MAX = 50;
 
-            /*Graph graph = new Graph(GRAPH_SIZE);
-            graph.Fill(MIN, MAX);*/
+            Graph graph = new Graph(GRAPH_SIZE);
+            graph.Fill(MIN, MAX);
 
-            Graph graph = new Graph(@"C:\Users\Artem\Desktop\ПА\Lab3\traveling salesman problem\task.dat");
+            //Graph graph = new Graph(@"C:\Users\Artem\Desktop\ПА\Lab3\traveling salesman problem\task.dat");
 
             Anthill anthill = new Anthill(graph);
 
             anthill.FindGoodTravel();
 
-            graph.Write(@"C:\Users\Artem\Desktop\ПА\Lab3\traveling salesman problem\task.dat");
-
-            
-
+            //graph.Write(@"C:\Users\Artem\Desktop\ПА\Lab3\traveling salesman problem\task.dat");         
             Console.ReadKey();
         }
     }
